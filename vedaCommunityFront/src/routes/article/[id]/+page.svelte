1ba4bf08ab64ac690a09a219b448dd2cf8e2cc56
YOUNGHO0 <31pqopq13@gmail.com>
2024-12-22 22:34:54 +0900

    Feat : 상세 페이지 생성

---
+++ vedaCommunityFront/src/routes/article/[id]/+page.svelte
@@ -0,0 +1,11 @@
+<!-- src/routes/article/[id]/+page.svelte -->
+<script>
+    // `data` 객체를 통해 `load` 함수에서 전달된 데이터를 가져옴
+    export let data;
+
+    // 필요한 데이터 추출
+    $: id = data.id;
+    console.log(id); // 디버깅: 제대로 된 값인지 확인
+</script>
+
+<h1>Article ID: {id}</h1>
