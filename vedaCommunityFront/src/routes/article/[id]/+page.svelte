9acb0fdfdd6c957d2b79809e71590cacf29a3480
YOUNGHO0 <31pqopq13@gmail.com>
2024-12-22 22:49:47 +0900

    Feat : 상세 페이지 생성

--- vedaCommunityFront/src/routes/article/[id]/+page.svelte
+++ vedaCommunityFront/src/routes/article/[id]/+page.svelte
@@ -1,11 +1,26 @@
 <!-- src/routes/article/[id]/+page.svelte -->
 <script>
     // `data` 객체를 통해 `load` 함수에서 전달된 데이터를 가져옴
-    export let data;
+    import {onMount} from "svelte";
 
-    // 필요한 데이터 추출
-    $: id = data.id;
-    console.log(id); // 디버깅: 제대로 된 값인지 확인
+    const apiUrl = import.meta.env.VITE_API_URL;
+    export let data;
+    let response ={};
+
+
+    onMount(async ()=>{
+
+        const apiResponse = await fetch(`${apiUrl}/api/v1/article/${data.id}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            credentials: "include", // 쿠키를 포함시켜 요청
+        });
+        response = await apiResponse.json();
+        console.log(response)
+
+        })
 </script>
 
-<h1>Article ID: {id}</h1>
+<h1>Article ID: {response.content}</h1>
