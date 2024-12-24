4ad7ec06b6df2efcf3da34111106318b11025e06
YOUNGHO0 <31pqopq13@gmail.com>
2024-12-24 16:47:31 +0900

    Fix : 삭제 버튼 추가

--- vedaCommunityFront/src/components/comment/ParentComment.svelte
+++ vedaCommunityFront/src/components/comment/ParentComment.svelte
@@ -14,19 +14,25 @@
         display: flex; /* Flexbox 활성화 */
         justify-content: center; /* 가로 가운데 정렬 */
         align-items: center; /* 세로 가운데 정렬 */
+        font-size: 12px;
     }
 </style>
 
-<div class="comment" style="padding:10px; margin: 5px">
+<div class="comment" style="padding:10px;">
     <div style="display: flex">
         <h4 style="margin: 5px; margin-left: 0px;">{comment.author.nickname}</h4>
         <div style="margin-left:auto " class="centered">{formatDateWithTime(comment.createdAt)}</div>
     </div>
-    <div style="display: flex">
-        <div class="comment-content" style="margin: 5 px; margin-left: 0px; padding-top: 10px">{comment.commentContent}</div>
+    <div style="padding-top: 10px;display: flex; align-items: center">
+        <div class="comment-content" style=" margin-left: 0px;">{comment.commentContent}</div>
         {#if $userStatus.isLoggedIn && $userStatus.userNickname === comment.author.nickname}
-        <Button onclick={()=>{editMode= !editMode}} style="margin-left: auto" secondary> 수정 </Button>
+        <div style="display: flex; align-items: center; margin-left: auto;justify-content: center">
+            <Button onclick={()=>{editMode= !editMode}} style="margin-left: auto" secondary> 수정 </Button>
+            <Button onclick={()=>{}} style="margin-left: auto" secondary> 삭제 </Button>
+        </div>
         {/if}
+
+
     </div>
 
 </div>
