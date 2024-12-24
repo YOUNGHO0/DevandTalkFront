abcd60ad057dc003de42ea7ec52af71e313835ce
YOUNGHO0 <31pqopq13@gmail.com>
2024-12-24 16:47:43 +0900

    Fix : 게시글 업테이트 추가

--- vedaCommunityFront/src/components/article/UpdateArticle.svelte
+++ vedaCommunityFront/src/components/article/UpdateArticle.svelte
@@ -29,12 +29,38 @@
     function flipEditMode(){
         editMode = !editMode;
     }
+    async function  updateArticle(id:bigint,title: string, content: string) {
+        const apiUrl = import.meta.env.VITE_API_URL;  // 환경변수에서 API URL 불러오기
 
+        // 서버로 전송할 데이터
+        const dto: App.ArticleUpdateDto = {
+            id: id,
+            title: title,
+            content: content,
+        };
 
-    let comments = [
-        { author: 'Alice', text: 'Great post!' },
-        { author: 'Bob', text: 'Thanks for sharing.' }
-    ];
+        try {
+            const apiResponse = await fetch(`${apiUrl}/api/v1/article`, {
+                method: "PATCH",  // POST 메서드로 요청
+                headers: {
+                    "Content-Type": "application/json",  // 요청 헤더에 Content-Type 설정
+                },
+                credentials: "include",  // 쿠키 포함
+                body: JSON.stringify(dto),  // request body에 데이터 추가 (JSON 형식)
+            });
+
+            // 응답 처리
+            if (!apiResponse.ok) {
+                throw new Error('Failed to create article');  // 요청 실패 시 오류 처리
+            }
+            if(apiResponse.status === 200){
+                window.history.back();
+            }
+        } catch (error) {
+            console.error("Error creating article:", error);
+            throw error;  // 오류 발생 시 처리
+        }
+    }
 </script>
 
 <style>
@@ -73,7 +99,7 @@
             </div>
         </div>
         <p style="margin-left: auto;width: 135px; display: flex; justify-content: space-between;">
-            <Button onclick={()=>{}} variant="raised"> 수정 </Button>
+            <Button onclick={()=>{updateArticle(response.id,editTitle,editConent)}} variant="raised"> 수정 </Button>
             <Button color="secondary"  onclick={()=>{editMode = !editMode}} variant="raised">취소</Button>
 
         </p>
