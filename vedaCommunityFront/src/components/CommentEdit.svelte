a16d0d66c8613f4be50285e266d85f27353614c3
YOUNGHO0 <31pqopq13@gmail.com>
2024-12-24 10:07:55 +0900

    Fix : 댓글 수정창 수정

--- vedaCommunityFront/src/components/CommentEdit.svelte
+++ vedaCommunityFront/src/components/CommentEdit.svelte
@@ -1,10 +1,30 @@
 <script lang="ts">
-    export let comment:App.CommentDto;
+    import Textfield from '@smui/textfield';
+    import HelperText from '@smui/textfield/helper-text';
 
+    export let comment:App.CommentDto;  // 부모에서 전달받은 comment 값
+    export let editMode:boolean;
+    import { writable } from 'svelte/store';
+    import Button from "@smui/button";
+
+    // $state는 상태값 관리용 스토어
+    let value = comment.commentContent // 부모로부터 전달받은 comment 값을 초기값으로 설정
+
+    function handleClick(event: MouseEvent) {
+        editMode = !editMode;
+    }
+    function sendEditComment(){};
 </script>
 
-
-
 <div>
-    댓글 수정창입니다.
+    <Textfield variant="outlined"
+               bind:value={value}
+               textarea
+               style="width: 100% ; height: 150px;
+               "
+    />
+    <Button onclick={sendEditComment} variant="raised"> 수정 </Button>
+    <Button color="secondary"  onclick={handleClick} variant="raised">취소</Button>
+
 </div>
+
