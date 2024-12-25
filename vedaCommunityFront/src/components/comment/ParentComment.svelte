4686524ed6deb6a8782d1680ca6751b31a5be4dd
YOUNGHO0 <31pqopq13@gmail.com>
2024-12-25 17:00:17 +0900

    Feat : 댓글 삭제 추가

--- vedaCommunityFront/src/components/comment/ParentComment.svelte
+++ vedaCommunityFront/src/components/comment/ParentComment.svelte
@@ -1,10 +1,42 @@
 
 <script lang="ts">
     import {formatDateWithTime} from "../../utils/helper.js";
-    import Button from "@smui/button";
+    import Button, {Label} from "@smui/button";
     import {userStatus} from "../../stores/user";
+    let open = $state(false);
+    import Dialog, { Title, Content, Actions } from '@smui/dialog';
+    let{applyMode= $bindable() ,fetchComments,comment,editMode = $bindable(false)} : {applyMode:boolean,fetchComments:()=>void,comment:App.CommentDto,editMode:boolean} = $props();
 
-    let{fetchComments,comment,editMode = $bindable(false)} : {fetchComments:()=>void,comment:App.CommentDto,editMode:boolean} = $props();
+    function flipApplyMode(){
+        applyMode = !applyMode;
+    }
+
+    async function deleteComment() {
+        const apiUrl = import.meta.env.VITE_API_URL;  // 환경변수에서 API URL 불러오기
+        try {
+            console.log("id " + comment.id);
+            const dto : App.CommentDeleteDto = { commentId: comment.id }
+            const apiResponse = await fetch(`${apiUrl}/api/v1/comment`, {
+                method: "DELETE",
+                headers: {
+                    "Content-Type": "application/json",  // 요청 헤더에 Content-Type 설정
+                },
+                credentials: "include",  // 쿠키 포함
+                body: JSON.stringify(dto),  // request body에 데이터 추가 (JSON 형식)
+            });
+
+            // 응답 처리
+            if (!apiResponse.ok) {
+                throw new Error('Failed to create article');  // 요청 실패 시 오류 처리
+            }
+            if(apiResponse.status === 200){
+                fetchComments();
+            }
+        } catch (error) {
+            console.error("Error creating article:", error);
+            throw error;  // 오류 발생 시 처리
+        }
+    }
 </script>
 
 
@@ -19,18 +51,24 @@
 
 <div class="comment" style="padding:10px;">
     <div style="display: flex">
-        <h4 style="margin: 5px; margin-left: 0px;">{comment.author.nickname}</h4>
+        <div style="justify-content:center;align-items: center; display: flex">
+            {#if comment.author != null}
+            <h4 style=" padding-top: 0px; margin:0px">{comment.author.nickname}</h4>
+            <Button onclick={flipApplyMode} style="padding-bottom: 1px;"> 답글 </Button>
+            {/if}
+
+
+        </div>
+        {#if comment.createdAt}
         <div style="margin-left:auto " class="centered">{formatDateWithTime(comment.createdAt)}</div>
+        {/if}
     </div>
     <div style="padding-top: 10px;display: flex; align-items: center">
         <div class="comment-content" style=" margin-left: 0px;">{comment.commentContent}</div>
-        {#if $userStatus.isLoggedIn && $userStatus.userNickname === comment.author.nickname}
+        {#if comment.author &&  $userStatus.isLoggedIn && $userStatus.userNickname === comment.author.nickname}
         <div style="display: flex; align-items: center; margin-left: auto;justify-content: center">
-            <Button onclick={()=>{
-                console.log("왜안돼");
-                console.log(editMode);
-                editMode= !editMode}} style="margin-left: auto" secondary> 수정 </Button>
-            <Button onclick={()=>{}} style="margin-left: auto" secondary> 삭제 </Button>
+            <Button onclick={()=>{editMode= !editMode}} style="margin-left: auto" secondary> 수정 </Button>
+            <Button onclick={()=>{open = true;}} style="margin-left: auto" secondary> 삭제 </Button>
         </div>
         {/if}
 
@@ -38,3 +76,21 @@
     </div>
 
 </div>
+
+
+<Dialog
+        bind:open
+        aria-labelledby="simple-title"
+        aria-describedby="simple-content"
+>
+    <!-- Title cannot contain leading whitespace due to mdc-typography-baseline-top() -->
+    <Content id="simple-content">정말로 댓글을 삭제하시겠습니까?</Content>
+    <Actions>
+        <Button>
+            <Label>No</Label>
+        </Button>
+        <Button onclick={deleteComment}>
+            <Label>Yes</Label>
+        </Button>
+    </Actions>
+</Dialog>
