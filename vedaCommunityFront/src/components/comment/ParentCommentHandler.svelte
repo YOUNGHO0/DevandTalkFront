63c1261621dfa4d2a8c0f2358126c1e4f801e26f
YOUNGHO0 <31pqopq13@gmail.com>
2024-12-25 17:00:37 +0900

    Feat : 자식 댓글 작성 추가

--- vedaCommunityFront/src/components/comment/ParentCommentHandler.svelte
+++ vedaCommunityFront/src/components/comment/ParentCommentHandler.svelte
@@ -4,31 +4,36 @@ import CommentEdit from "./CommentEdit.svelte";
 import ParentComment from "./ParentComment.svelte";
 import {on} from "svelte/events";
 import ChildCommentHandler from "./ChildCommentHandler.svelte";
+import ChildCommentWrite from "./ChildCommentWrite.svelte";
 
 let editMode :boolean =$state(false);
+let applyMode : boolean = $state(false);
+let {fetchComments,comment = $bindable()} : {fetchComments:()=> void, comment : App.CommentDto } = $props();
 
-let {fetchComments,comment} : {fetchComments:()=> void, comment : App.CommentDto } = $props();
-
-let childCommentList :App.CommentDto[]|null = comment.childCommentList;
+let commentState = $state(comment)
+let childCommentList :App.CommentDto[]|null = comment.childCommentList
 
 </script>
 
 <div>
     {#if editMode}
         <div style="display:flex;">
-            <CommentEdit {fetchComments} {comment} bind:editMode></CommentEdit>
+            <CommentEdit bind:fetchComments {comment} bind:editMode></CommentEdit>
         </div>
 
 
     {:else}
-        <ParentComment {fetchComments} {comment} bind:editMode></ParentComment>
+        <ParentComment bind:applyMode {fetchComments} {comment} bind:editMode></ParentComment>
     {/if}
 
     {#if childCommentList !== null}
         {#each childCommentList as childComment}
-            <ChildCommentHandler {fetchComments} {childComment}></ChildCommentHandler>
+            <ChildCommentHandler bind:applyMode {fetchComments} {childComment}></ChildCommentHandler>
         {/each}
     {:else}
+    {/if}
+    {#if applyMode}
+        <ChildCommentWrite bind:applyMode id={comment.id} {fetchComments}></ChildCommentWrite>
     {/if}
 </div>
 
