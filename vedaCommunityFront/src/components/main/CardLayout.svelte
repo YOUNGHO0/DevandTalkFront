685fdf25402c86d47b486c583fb044292a77d76d
YOUNGHO0 <31pqopq13@gmail.com>
2024-12-26 21:06:59 +0900

    Fix : 메인 레이아웃 추가

---
+++ vedaCommunityFront/src/components/main/CardLayout.svelte
@@ -0,0 +1,31 @@
+
+<script lang="ts">
+
+    import {formatDate} from "../../utils/helper.js";
+    import "../../utils/common.css"
+    let {dataList, title, type}:{ dataList:App.MainArticle[],title:string,type:string} = $props();
+    import List, { Item, Text, Graphic, Separator, Subheader } from '@smui/list';
+    import {goto} from "$app/navigation";
+    import Button from "@smui/button";
+    const apiUrl = import.meta.env.VITE_API_URL;
+
+</script>
+
+
+<div class="comment" style="width: 400px">
+    <div style="display: flex; align-items: center; justify-content: center; padding: 10px">
+        <h2 style="font-size: 20px; margin: 0px;      ">{title}</h2>
+        <Button style="margin-left: auto" variant="outlined">More</Button>
+    </div>
+
+    <Separator></Separator>
+    {#each dataList as data}
+        <div onclick={()=>{goto(`/${type}/${data.id}`)}}  style="display: flex;justify-content: flex-start; padding: 10px;align-items: center;">
+            <div style="font-size: 15px;"> {formatDate(data.createdDate)} </div>
+            <div style="font-size: 20px; margin-left: 20px"> {data.title} </div>
+        </div>
+        <Separator></Separator>
+
+    {/each}
+
+</div>
--- vedaCommunityFront/src/routes/+page.svelte
+++ vedaCommunityFront/src/routes/+page.svelte
@@ -1,1 +1,47 @@
-<div> hello this iS svelt page</div>
+
+
+<script lang="ts">
+    import {onMount} from "svelte";
+    import {get} from "svelte/store";
+    import CardLayout from "../components/main/CardLayout.svelte";
+
+    const apiUrl = import.meta.env.VITE_API_URL;
+
+    let articleData : App.MainArticle[] = $state([]);
+    let anonArticleData : App.MainArticle[] = $state([]);
+    // async/await를 사용하여 비동기적으로 데이터를 가져오고 Promise를 반환
+    async function getArticleList(type: string): Promise<App.MainArticle[]> {
+        try {
+            const apiResponse = await fetch(`${apiUrl}/api/v1/${type}/list?size=10`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                credentials: "include", // 쿠키를 포함시켜 요청
+            });
+
+            // JSON 데이터를 가져와 articleData에 저장
+            let data ;// Svelte store에 배열 타입을 지정
+            data = await apiResponse.json();
+            console.log(data);
+            return data.content; // 비동기 함수는 Promise를 반환해야 하므로 이를 반환
+        } catch (e) {
+            console.log("예외발생", e);
+            return [];  // 에러가 발생하면 빈 배열을 반환
+        }
+    }
+
+    onMount(async()=>{
+        articleData = await getArticleList("article");
+        anonArticleData = await getArticleList("anonArticle")
+    })
+</script>
+
+
+
+<div
+        style="display: flex; flex-wrap: wrap; gap: 20px; justify-content: center;">
+    <!-- CardLayout 컴포넌트를 2개 나란히 배치하며, 화면이 좁아지면 아래로 이동 -->
+    <CardLayout type="article" title="자유게시판" dataList={articleData} style="flex: 1 1 300px;"/>
+    <CardLayout type="anonArticle" title="익명게시판" dataList={anonArticleData} style="flex: 1 1 300px;"/>
+</div>
