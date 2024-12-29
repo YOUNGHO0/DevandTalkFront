b2f5a1063f0bfeeb989e839fbfe5ac9a5fa44ecb
YOUNGHO0 <31pqopq13@gmail.com>
2024-12-29 14:54:10 +0900

    Fix : 로그인 ui 변경

--- vedaCommunityFront/src/routes/login/+page.svelte
+++ vedaCommunityFront/src/routes/login/+page.svelte
@@ -1,7 +1,19 @@
 
 <script>
+
+    import {goto} from "$app/navigation";
+
     const apiUrl = import.meta.env.VITE_API_URL;
 </script>
 
 
-<a href={`${apiUrl}/oauth2/authorization/google`}>로그인 해주세요</a>
+
+
+
+<div style=" width:300px; justify-content: center; align-items: center; margin-left: auto; margin-right: auto; ">
+    <h2 >로그인 해주세요</h2>
+    <a href={`${apiUrl}/oauth2/authorization/google`} >
+    <img style="width: 300px; " src="/google.png" onclick={()=>{console.log("wht")}}/>
+    </a>
+</div>
+
